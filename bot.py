70e265769603bbe70b75cdc7c8a23bc4a591ef5e
Sanika Chavan <chavansanikavikas@gmail.com>
2020-06-22 00:34:19 +0530

    deploy bot to heroku

--- bot.py
+++ bot.py
@@ -204,7 +204,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
     updater.bot.setWebhook('https://sheltered-shore-73299.herokuapp.com/ ' + TOKEN)
 
-    updater.start_polling()
+#    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
